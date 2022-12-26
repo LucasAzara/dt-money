183ebb3f394ec5f77dc69b445af9f284b101cce1
Lucas de Souza Azara <lucas.s.azara@gmail.com>
2022-12-26 20:10:15 -0300

    React Form Hooks and Zod

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,5 @@
+// Radix UI
 import * as Dialog from '@radix-ui/react-dialog'
-
 // Icons
 import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
 // CSS
@@ -10,8 +10,35 @@ import {
   TransactionType,
   TransactionTypeButton,
 } from './styles'
+// Zod & React Form Hook
+import * as z from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+// Zod Schema
+const newTransactionFormSchema = z.object({
+  description: z.string(),
+  price: z.number(),
+  category: z.string(),
+})
+
+type TTransactionFormSchema = z.infer<typeof newTransactionFormSchema>
 
 export function NewTransactionModal() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<TTransactionFormSchema>({
+    resolver: zodResolver(newTransactionFormSchema),
+  })
+
+  async function handleCreateNewTransaction(data: TTransactionFormSchema) {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    console.log(data)
+  }
+
   return (
     <Dialog.Portal>
       <Overlay />
@@ -23,10 +50,25 @@ export function NewTransactionModal() {
           <X size={24} />
         </CloseButton>
 
-        <form>
-          <input type="text" placeholder="Descrição" required />
-          <input type="number" placeholder="Preço" required />
-          <input type="text" placeholder="Categoria" required />
+        <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
+          <input
+            type="text"
+            placeholder="Descrição"
+            required
+            {...register('description')}
+          />
+          <input
+            type="number"
+            placeholder="Preço"
+            required
+            {...register('price', { valueAsNumber: true })}
+          />
+          <input
+            type="text"
+            placeholder="Categoria"
+            required
+            {...register('category')}
+          />
 
           <TransactionType>
             <TransactionTypeButton variant="income" value="income">
@@ -39,7 +81,9 @@ export function NewTransactionModal() {
             </TransactionTypeButton>
           </TransactionType>
 
-          <button type="submit">Cadastrar</button>
+          <button type="submit" disabled={isSubmitting}>
+            Cadastrar
+          </button>
         </form>
       </Content>
     </Dialog.Portal>
--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,12 +1,46 @@
+// Icon
 import { MagnifyingGlass } from 'phosphor-react'
+// CSS
 import { SearchFormContainer } from './styles'
+// Zod & React Form Hook
+import * as z from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+// Zod Schema
+const searchFormSchema = z.object({
+  query: z.string(),
+})
+
+// Zod type
+type TSearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
-  return (
-    <SearchFormContainer>
-      <input type="text" placeholder="Busque por transações" />
+  // React Hook Form & Zod Verification and Submit function
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<TSearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+  })
 
-      <button type="submit">
+  // Action on Submit
+  async function handleSearchTransactions(data: TSearchFormInputs) {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    console.log(data)
+  }
+
+  return (
+    <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+      <input
+        type="text"
+        placeholder="Busque por transações"
+        {...register('query')}
+      />
+
+      <button type="submit" disabled={isSubmitting}>
         <MagnifyingGlass size={20} />
         Buscar
       </button>
